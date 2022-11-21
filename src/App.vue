e89792b63532b81ecd9f55f5a220821560e12efd
mateus-agostinho <mateusagostinho123@gmail.com>
2022-11-21 07:35:50 -0500

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <a href="#" class="logo"><i class="fas fa-utensils"></i>Food</a>
+
+      <nav class="navbar">
+        <a href="#home">Home </a>
+        <a href="#Special">Especial </a>
+        <a href="#popular">Popular </a>
+      </nav>
+
+    </header>
+
+
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  font-family: 'Nunito', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+  text-decoration: none;
+  text-transform: capitalize;
+  transition: all .2s linear;
 }
+body{
+  background: #f7f7f7 ;
+}
+header{
+  position: fixed;
+  top: 0; 
+  left: 0; 
+  right: 0;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #fff;
+  padding: 2rem 9%;
+  box-shadow:0 .5rem 1rem rgb(0,0,0, .1) ;
+}
+header .logo{
+  font-size: 2.5rem;
+  font-weight: bolder;
+  color: #666;
+}
+header .logo i{
+  padding-right: .5rem;
+  color: rgb(134, 7, 7);
+}
+
 </style>
